725ee67e425b69be72ac02696604ab947490072f
Niklaus-Mikael <somnath.revolite@gmail.com>
2021-08-27 15:50:42 +0530

    added data to template

--- src/app/Components/weather/weather.component.ts
+++ src/app/Components/weather/weather.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { WeatherApiService } from 'src/app/Services/weather-api.service';
-import { Report } from './model/weather';
-import { Observable } from 'rxjs';
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
@@ -10,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 
 export class WeatherComponent implements OnInit {
-data$ !: Observable<Report>;
+public data !: any;
 icon!:string;
   constructor(private service:WeatherApiService) { }
 searchItem:string="";
@@ -20,25 +18,7 @@ searchItem:string="";
   }
 
   onSubmit(){
-   this.data$ = this.service.getWeather(this.searchItem)
-/* 
-
-  let {location:{name,country,region,localtime}}=this.data;
-  let {current:{temperature,weather_icons,weather_description,humidity,wind_dir}}=this.data
-  this.current.temperature=temperature;
-  this.current.humidity=humidity;
-  this.current.weather_descriptions=weather_description[0];
-  this.current.weather_icons=weather_icons[0];
-  this.current.wind_dir=wind_dir;
-  this.location.country=country;
-  this.location.localtime=localtime;
-  this.location.name=name;
-  this.location.region=region;  */
-
-
-  
-
-
+   this.service.getWeather(this.searchItem).subscribe((data)=>{this.data=data;console.log(this.data)});
   }
 
 }
--- src/app/Services/weather-api.service.ts
+++ src/app/Services/weather-api.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Report } from '../Components/weather/model/weather';
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherApiService {
 
+  report$ !:Observable<any>;
   constructor(private http:HttpClient) { }
 
    API_Key='6f06a0c24f45e1c684316c32e47d2c02';
@@ -14,7 +14,7 @@ export class WeatherApiService {
 
   getWeather(city:string):Observable<any>{
     let API_Url=`http://api.weatherstack.com/current?access_key=${this.API_Key}&query=${city}`;
-    return this.http.get<any>(API_Url);
+    return  this.http.get<any>(API_Url);
   }
 
 }
